2f3daa28249d1e756081032e587f60c00d0e474c
Peven <iampeven@gmail.com>
2018-02-17 03:12:01 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 # !/bin/bash
 
 fdisk -l
-read -p "Fresh machine?(type y we will create partition automaticly[or n]): " fresh 
+
+read -p "Fresh machine?(type y we will create partition automaticly [ or n ]): " fresh 
 read -p "Type the disk you want to install[like: /dev/sda]: " disk
-if [ ${fresh} == 'y' ]
+if [ ${fresh} == "y" ]
     then
-    parted ${disk} -s mklabel gpt mkpart ESP fat32 1M 513M mkpart primary ext4 513M 100% 
+    parted ${disk} -s mklabel gpt mkpart ESP fat32 1M 513M mkpart primary ext4 514M 100%
+    set 1 boot on
+    mkfs.vfat -F32 ${disk}1 
+    mkfs.ext4 ${disk}2
     mount ${disk}2 /mnt 
     mkdir /mnt/boot
-    mount /dev/sda1 /mnt/boot 
+    mount ${disk}1 /mnt/boot 
     else
     read -p "Type the partition you want to install(like :/dev/sda2): " partition
+    mkfs.ext4 ${partition}
     mount ${partition} /mnt 
     mkdir /mnt/boot
     mount /dev/sda1 /mnt/boot 
@@ -19,47 +24,48 @@ read -p "Type your hostname(like:Arch): " hostname
 read -p "Type the username you want to create: " username
 read -p "Type the password: " password
 read -p "Are you from China?(type y we will replace the mirrorlist[or n]) :" china 
-if [ ${china} == 'y' ]
+if [ ${china} == "y" ]
     then
     mv mirrorlist /etc/pacman.d/mirrorlist
+    echo "\nen_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8\nzh_TW.UTF-8 UTF-8" >> /mnt/etc/locale.genfstab
+    else 
+    echo "\nen_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen 
 fi
 
-pacstrap -i /mnt base base-devel
+pacman -Syy
+pacstrap -i /mnt base base-devel --noconfirm
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt 
-if [ ${china} == 'y' ]
-    then
-    echo "\nen_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8\nzh_TW.UTF-8 UTF-8" >> /etc/locale.genfstab
-    else 
-    echo "\nen_US.UTF-8 UTF-8" >> /etc/locale.gen 
-fi
-locale-gen 
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-echo ${hostname} > /etc/hostname
+# in arch-chroot
+
+arch-chroot /mnt locale-gen 
+arch-chroot /mnt echo LANG=en_US.UTF-8 > /etc/locale.conf
+arch-chroot /mnt hostname ${hostname}
+arch-chroot /mnt echo ${hostname} > /etc/hostname
 
 #grub
-pacman -S intel-ucode os-prober grub efibootmgr --noconfirm
+arch-chroot /mnt pacman -S --noconfirm intel-ucode os-prober grub efibootmgr 
 #to use wifi-menu
-pacman -S dialog wpa_supplicant --noconfirm
+arch-chroot /mnt pacman -S --noconfirm dialog wpa_supplicant 
 #install base DE
-pacman -S xorg xorg-xinit xf86-video-intel libinput alsa-utils pulseaudio\
-mesa-demos  xfce4 xfce4-goodies lightdm lightdm-gtk-greeter gtk-engine-murrine\
-udisks2 ntfs-3g gvfs networkmanager gnome-keyring libsecret network-manager-applet\
-xfce4-notifyd ttf-dejavu wqy-microhei file-roller --noconfirm
+arch-chroot /mnt pacman -S --noconfirm xorg xorg-xinit xf86-video-intel libinput alsa-utils pulseaudio
+arch-chroot /mnt pacman -S --noconfirm mesa-demos  xfce4 xfce4-goodies lightdm lightdm-gtk-greeter gtk-engine-murrine
+arch-chroot /mnt pacman -S --noconfirm udisks2 ntfs-3g gvfs networkmanager gnome-keyring libsecret network-manager-applet
+arch-chroot /mnt pacman -S --noconfirm xfce4-notifyd ttf-dejavu wqy-microhei file-roller 
 #install applications
-pacman -S firefox vlc thunderbird
+arch-chroot /mnt pacman -S  --noconfirm firefox vlc thunderbird 
 #create user
-useradd -m -g users -G wheel -s /bin/bash ${username}
-(echo ${password};sleep 1;echo ${password}) | passwd ${username} > /dev/null
+arch-chroot /mnt useradd -m -g users -G wheel -s /bin/bash ${username}
+arch-chroot /mnt echo ${username}:${password} | chpasswd
 #configure system
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch --recheck
-grub-mkconfig -o /boot/grub/grub.cfg 
-systemctl enable NetworkManager 
-systemctl enable lightdm 
+arch-chroot /mnt systemctl enable NetworkManager 
+arch-chroot /mnt systemctl enable lightdm 
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch --recheck
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg 
+
 
 #reboot
 echo "Completing..."
-exit 
 umount -R /mnt
 reboot 
+
 
